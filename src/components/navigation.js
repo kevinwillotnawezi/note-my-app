e47fd7915764ac67c6ebecc6928bf3d23d75c345
Kevin Willot <kevin.willot.nawezi@gmail.com>
2021-12-24 02:10:58 +0100

    add menu and home page

---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+function Menu() {
+	return (
+		<nav>
+			<ul>
+				<li>Note</li>
+				<li>All comments</li>
+				<li>All comments</li>
+			</ul>
+		</nav>
+	);
+}
+export default Menu;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,7 @@
+import './home.css';
+
+function Home() {
+	return <div className='App'></div>;
+}
+
+export default Home;
