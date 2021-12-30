b8b09174029504f5b92ae8a0f60d1d84cf736cd9
Kevin Willot <kevin.willot.nawezi@gmail.com>
2021-12-30 01:39:48 +0100

    remove packages and add images

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,7 +1,8 @@
 export const basedUrlApi = 'http://localhost:4000/api/';
-export const urlApiComments = basedUrlApi + 'comment/feedback';
-export const urlApiUsers = basedUrlApi + 'user';
-export const urlApiFeedbacks = basedUrlApi + 'feedback';
-export const urlApiNotations = basedUrlApi + 'notation';
-export const urlApiNotationsFeedback = urlApiNotations + '/feedback';
-export const urlApiNotationsComments = urlApiNotations + '/comment';
+export const urlApiComments = basedUrlApi + 'comment/';
+export const urlApiCommentsFeedback = urlApiComments + 'feedback/';
+export const urlApiUsers = basedUrlApi + 'user/';
+export const urlApiFeedbacks = basedUrlApi + 'feedback/';
+export const urlApiNotations = basedUrlApi + 'notation/';
+export const urlApiNotationsFeedback = urlApiNotations + 'feedback/';
+export const urlApiNotationsComments = urlApiNotations + 'comment/';
