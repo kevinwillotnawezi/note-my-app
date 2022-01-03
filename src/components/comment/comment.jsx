53ece3ec6a4b100f53d1f057ba70d0c575d52965
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-01-03 21:05:36 +0100

    add comment component and test

---
+++ src/components/comment/comment.jsx
@@ -0,0 +1,113 @@
+import colors from '../../assets/styles/colors';
+import { MyForm, MyTextArea } from '../styledComponents/form/form';
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import useFetch from 'use-http';
+import { urlApiComments } from '../../util/constants';
+import { userId } from './../../util/constants';
+import { useState } from 'react';
+import { LoaderWrapper } from '../styledComponents/loader/loader';
+import { Loader } from './../styledComponents/loader/loader';
+
+const Accordion = styled.button`
+	background-color: ${colors.green};
+	color: white;
+	cursor: pointer;
+	padding: 18px;
+	text-align: left;
+	border: none;
+	outline: none;
+	transition: 0.4s;
+
+	&:hover,
+	&:active {
+		background-color: ${colors.grey_blue};
+		color: ${colors.dark};
+	}
+`;
+
+const Panel = styled.div`
+	padding: 0 18px;
+	background-color: white;
+	display: none;
+	overflow: hidden;
+	transition: max-height 0.2s ease-out;
+`;
+
+function handleAccordion() {
+	const accordion = document.getElementById('Accordion');
+	accordion?.classList.toggle('active');
+	const panel = document.getElementById('Panel');
+	if (panel?.style.display === 'block') {
+		panel.style.display = 'none';
+	} else {
+		panel.style.display = 'block';
+	}
+}
+
+function Comment({ feedbackId, data, setData }) {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const { post, response, error } = useFetch(urlApiComments);
+	const [submit, setSubmit] = useState(false);
+	const [isLoading, setLoading] = useState(false);
+
+	async function onSubmit(comment) {
+		const body = {
+			...comment,
+			userId,
+			feedbackId,
+		};
+		setSubmit(true);
+		setLoading(true);
+		await post('', body);
+		handleAccordion();
+		if (response.ok) {
+			const newData = [
+				...data,
+				{
+					...comment,
+					userId,
+					feedbackId,
+				},
+			];
+			setData(newData);
+		}
+		setLoading(false);
+	}
+
+	return (
+		<div>
+			<Accordion data-testid='accordionButton' onClick={handleAccordion}>
+				Add new Comment
+			</Accordion>
+			<Panel id='Panel' data-testid='panel'>
+				<MyForm onSubmit={handleSubmit(onSubmit)}>
+					<label>Comment :</label>
+					{errors.description && (
+						<p className='error' data-testid='error'>
+							This field is required
+						</p>
+					)}
+					<MyTextArea data-testid='text-area' placeholder='Write something..' {...register('description', { required: true })} />
+					<input data-testid='submitButton' type='submit' value='Submit' />
+				</MyForm>
+			</Panel>
+			{submit &&
+				(isLoading ? (
+					<LoaderWrapper>
+						<Loader data-testid='loader' />
+					</LoaderWrapper>
+				) : error ? (
+					<div>An error occured</div>
+				) : (
+					<div>Comment submitted</div>
+				))}
+		</div>
+	);
+}
+
+export default Comment;
---
+++ src/components/comment/comment.test.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { screen, render, fireEvent, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import colors from '../../assets/styles/colors';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { urlApiComments } from '../../util/constants';
+import Comment from './comment';
+
+//to test error message uncomment below
+// const server = setupServer(
+// 	rest.post(urlApiComments, (req, res, ctx) => {
+// 		return res(ctx.status(400));
+// 	})
+// );
+
+//to test error message comment below
+const server = setupServer(
+	rest.post(urlApiComments, (req, res, ctx) => {
+		return res(
+			ctx.json({
+				message: 'Comment created successfully',
+			})
+		);
+	})
+);
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('Comment', () => {
+	it('should render the all the inputs', () => {
+		render(<Comment />);
+		const accordion = screen.getByTestId('accordionButton');
+
+		//before click on accordion
+		expect(accordion).toBeInTheDocument();
+		expect(screen.queryByTestId('panel')).toHaveStyle('display: none');
+
+		//after click on accordion
+		fireEvent.click(accordion);
+		expect(screen.queryByTestId('panel')).toHaveStyle('display: block');
+
+		//after second click on accordion
+		fireEvent.click(accordion);
+		expect(screen.queryByTestId('panel')).toHaveStyle('display: none');
+	});
+
+	it('should display error when trying to submit when field missing', async () => {
+		render(<Comment />);
+		const submit = screen.getByTestId('submitButton');
+
+		//before click on submit
+		expect(screen.queryByText('This field is required')).toBeFalsy();
+
+		//after click on submit
+		fireEvent.click(submit);
+		await waitFor(() => {
+			const error = screen.getByText('This field is required');
+			expect(error).toBeInTheDocument();
+		});
+	});
+
+	// to test error message uncomment below
+
+	// it('should display message when submit has error', async () => {
+	// 	render(<Comment />);
+	// 	const submit = screen.getByTestId('submitButton');
+	// 	const textArea = screen.getByTestId('text-area');
+
+	// 	//before click on submit
+	// 	expect(screen.queryByTestId('An error occured')).toBeFalsy();
+
+	// 	//after click on submit
+	// 	fireEvent.change(textArea, { target: { value: 'abc' } });
+	// 	fireEvent.click(submit);
+
+	// 	await waitFor(() => {
+	// 		const errorMessage = screen.queryByText('An error occured');
+	// 		expect(errorMessage).toBeInTheDocument();
+	// });
+	// });
+
+	it('should display message when submit is successful', async () => {
+		render(<Comment />);
+		const submit = screen.getByTestId('submitButton');
+		const textArea = screen.getByTestId('text-area');
+
+		//before click on submit
+		expect(screen.queryByTestId('Comment submitted')).toBeFalsy();
+
+		//after click on submit
+		fireEvent.change(textArea, { target: { value: 'abc' } });
+		fireEvent.click(submit);
+
+		await waitFor(async () => {
+			const loader = screen.queryByTestId('loader');
+			expect(loader).toBeInTheDocument();
+		});
+	});
+});
