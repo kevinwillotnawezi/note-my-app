757b077c0b00e1a0c30db5549bbeb249a09d68cf
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-01-03 22:37:18 +0100

    delete log

--- src/util/useFetch.jsx
+++ src/util/useFetch.jsx
@@ -14,7 +14,6 @@ export function useFetch(url) {
 				const data = await response.json();
 				setData(data);
 			} catch (err) {
-				console.err(err);
 				setError(true);
 			} finally {
 				setLoading(false);
