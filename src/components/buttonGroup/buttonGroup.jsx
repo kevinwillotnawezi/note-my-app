166dc84b8ea10d744f07f9435f55f9b803a5c723
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-01-03 18:30:25 +0100

    add feedback page and butto component

---
+++ src/components/buttonGroup/buttonGroup.jsx
@@ -0,0 +1,60 @@
+import '../../assets/styles/index.css';
+import styled from 'styled-components';
+import colors from '../../assets/styles/colors';
+
+const possibleValues = [1, 2, 3, 4, 5];
+
+const GroupButton = styled.div`
+	display: flex;
+	align-items: center;
+
+	input[type='radio'] {
+		position: absolute;
+		visibility: hidden;
+	}
+
+	label {
+		background-color: ${colors.secondary};
+		border: 1px solid ${colors.dark};
+		color: ${colors.dark};
+		padding: 10px 24px !important;
+		cursor: pointer;
+	}
+
+	label:not(#label1) {
+		border-left: none;
+	}
+
+	label:hover {
+		background-color: ${colors.green};
+		color: white;
+	}
+
+	input[type='radio']:checked + label {
+		background-color: ${colors.green} !important;
+		color: white !important;
+	}
+`;
+
+function ButtonGroup({ register }) {
+	return (
+		<div className='container'>
+			<GroupButton>
+				<span>Bad</span>
+				<div className='btn-group'>
+					{possibleValues.map((value) => (
+						<span name={'radio' + value} key={value}>
+							<input data-testid={'radio' + value} {...register('rating', { required: true })} type='radio' value={value} id={'radio' + value} />
+							<label htmlFor={'radio' + value} id={'label' + value}>
+								{value}
+							</label>
+						</span>
+					))}
+				</div>
+				<span>Good</span>
+			</GroupButton>
+		</div>
+	);
+}
+
+export default ButtonGroup;
---
+++ src/pages/newFeedback/newFeedback.jsx
@@ -0,0 +1,68 @@
+import '../../assets/styles/index.css';
+import { MyForm, MyTextArea } from '../../components/styledComponents/form/form';
+import ButtonGroup from '../../components/buttonGroup/buttonGroup';
+import { useForm } from 'react-hook-form';
+import { usePost } from '../../util/useFetch';
+import { urlApiFeedbacks } from '../../util/constants';
+import { Loader, LoaderWrapper } from '../../components/styledComponents/loader/loader';
+import { useState } from 'react';
+
+function Feedback() {
+	const userId = 'test';
+	const category = null;
+	const status = 'open';
+	const [submit, setSubmit] = useState(false);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const { isLoading, error, setBody } = usePost(urlApiFeedbacks);
+
+	function onSubmit(data) {
+		console.log(submit);
+		const body = {
+			...data,
+			userId,
+			category,
+			status,
+		};
+		setBody(body);
+		setSubmit(true);
+	}
+
+	return (
+		<div className='container'>
+			<h1>New Feedback</h1>
+			<MyForm onSubmit={handleSubmit(onSubmit)}>
+				<label>Rating :</label>
+				{errors.rating && (
+					<p className='error' data-testid='error1'>
+						This field is required
+					</p>
+				)}
+				<ButtonGroup register={register} />
+				<label>Note :</label>
+				{errors.note && (
+					<p className='error' data-testid='error2'>
+						This field is required
+					</p>
+				)}
+				<MyTextArea data-testid='text-area' placeholder='Write something..' {...register('note', { required: true, maxLength: 200 })}></MyTextArea>
+				<input aria-label='submit-button' type='submit' value='Submit' />
+			</MyForm>
+			{submit &&
+				(isLoading ? (
+					<LoaderWrapper>
+						<Loader data-testid='loader' />
+					</LoaderWrapper>
+				) : error ? (
+					<div>An error occured</div>
+				) : (
+					<div>Feedback submitted</div>
+				))}
+		</div>
+	);
+}
+
+export default Feedback;
