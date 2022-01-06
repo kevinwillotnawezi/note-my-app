89aa6161a75ccadc1fa79c830396ddc6d91c16eb
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-01-06 07:25:54 +0100

    update api url

--- src/util/constants.js
+++ src/util/constants.js
@@ -2,7 +2,9 @@
 export const basedUrlApi = 'http://localhost:4000/api/';
 export const urlApiComments = basedUrlApi + 'comment/';
 export const urlApiCommentsFeedback = urlApiComments + 'feedback/';
-export const urlApiUsers = basedUrlApi + 'user/';
+export const urlApiUsers = basedUrlApi + 'auth/';
+export const urlApiSignUp = urlApiUsers + 'signup/';
+export const urlApiLogin = urlApiUsers + 'login/';
 export const urlApiFeedbacks = basedUrlApi + 'feedback/';
 export const urlApiNotations = basedUrlApi + 'notation/';
 export const urlApiNotationsFeedback = urlApiNotations + 'feedback/';
@@ -10,3 +12,5 @@ export const urlApiNotationsComments = urlApiNotations + 'comment/';
 
 //TODO change here when user connection is handled
 export const userId = 'userTest';
+
+//TODO internationalization
