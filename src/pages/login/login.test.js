827d84699602348cebd47cff55efd6c183be2c23
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-01-06 20:58:51 +0100

    update tests login

--- src/pages/login/login.test.js
+++ src/pages/login/login.test.js
@@ -3,45 +3,24 @@ import { screen, render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { urlApiFeedbacks } from '../../util/constants';
+import { urlApiLogin } from '../../util/constants';
 import Login from './login';
 
-//to test existing email uncomment below
-//TODO check below
-// const server = setupServer(
-// 	rest.post(urlApiFeedbacks, (req, res, ctx) => {
-// 		return res(
-// 			ctx.status(400),
-// 			ctx.json({
-// 				message: 'User validation failed: email: Error, expected `email` to be unique. Value: `admin@admin`',
-// 			})
-// 		);
-// 	})
-// );
-
-// const server = setupServer(
-// 	rest.post(urlApiFeedbacks, (req, res, ctx) => {
-// 		return res(ctx.status(500));
-// 	})
-// );
-
-//to test error message comment below
-//TODO see test here (and in other files)
-const server = setupServer(
-	rest.post(urlApiFeedbacks, (req, res, ctx) => {
-		return res(
-			ctx.status(201),
-			ctx.json({
-				message: 'User created successfully',
-			})
-		);
-	})
-);
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
 describe('Signup', () => {
+	const server = setupServer(
+		rest.post(urlApiLogin, (req, res, ctx) => {
+			return res(
+				ctx.status(200),
+				ctx.json({
+					message: 'Login successful',
+				})
+			);
+		})
+	);
+	beforeAll(() => server.listen());
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
+
 	it('should only render all the inputs and buttons', () => {
 		render(<Login />);
 		expect(screen.getByTestId('email')).toBeInTheDocument();
@@ -65,37 +44,6 @@ describe('Signup', () => {
 		expect(screen.getAllByText('This field is required')).toHaveLength(2);
 	});
 
-	it('should display an error message when email is not valid', async () => {
-		render(<Login />);
-
-		const submitButton = screen.getByTestId('submit');
-		const email = screen.getByTestId('email');
-		fireEvent.change(email, { target: { value: 'abc' } });
-		fireEvent.click(submitButton);
-
-		await waitFor(() => {
-			expect(screen.getByTestId('error-email')).toBeInTheDocument();
-		});
-		expect(screen.getByText('Invalid email')).toBeInTheDocument();
-	});
-
-	//uncomment this part and the part at the top of the file to test error message
-	it('should display an error when email is unknown', async () => {
-		render(<Login />);
-
-		const submitButton = screen.getByTestId('submit');
-		const email = screen.getByTestId('email');
-		fireEvent.change(email, { target: { value: 'test@test' } });
-		const password = screen.getByTestId('password');
-		fireEvent.change(password, { target: { value: '12345678' } });
-		fireEvent.click(submitButton);
-
-		await waitFor(() => {
-			expect(screen.getByText('Unknown user')).toBeInTheDocument();
-		});
-		//TODO => forgot password
-	});
-
 	it('should display a message when the user is created', async () => {
 		render(<Login />);
 
@@ -108,7 +56,94 @@ describe('Signup', () => {
 
 		//TODO check when we change pages?
 		await waitFor(() => {
-			expect(screen.getByText('Login sucessful')).toBeInTheDocument();
+			expect(screen.getByText('Login successful')).toBeInTheDocument();
+		});
+	});
+});
+
+describe('Singup invalid password', () => {
+	const server = setupServer(
+		rest.post(urlApiLogin, (req, res, ctx) => {
+			return res(
+				ctx.status(401),
+				ctx.json({
+					message: 'Invalid password',
+				})
+			);
+		})
+	);
+	beforeAll(() => server.listen());
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
+
+	it('should display an error message when password is not valid', async () => {
+		render(<Login />);
+		const submitButton = screen.getByTestId('submit');
+		const email = screen.getByTestId('email');
+		fireEvent.change(email, { target: { value: 'abc@abc' } });
+		const password = screen.getByTestId('password');
+		fireEvent.change(password, { target: { value: '12345678' } });
+		fireEvent.click(submitButton);
+		await waitFor(() => {
+			expect(screen.getByText('Invalid password')).toBeInTheDocument();
+		});
+	});
+});
+
+describe('Singup unknonw user', () => {
+	const server = setupServer(
+		rest.post(urlApiLogin, (req, res, ctx) => {
+			return res(
+				ctx.status(404),
+				ctx.json({
+					message: 'User not found ',
+				})
+			);
+		})
+	);
+	beforeAll(() => server.listen());
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
+
+	it('should display an error when email is unknown', async () => {
+		render(<Login />);
+
+		const submitButton = screen.getByTestId('submit');
+		const email = screen.getByTestId('email');
+		fireEvent.change(email, { target: { value: 'test@test' } });
+		const password = screen.getByTestId('password');
+		fireEvent.change(password, { target: { value: '12345678' } });
+		fireEvent.click(submitButton);
+
+		await waitFor(() => {
+			expect(screen.getByText('User not found')).toBeInTheDocument();
+		});
+		//TODO => forgot password
+	});
+});
+
+describe('Singup unknonw error', () => {
+	const server = setupServer(
+		rest.post(urlApiLogin, (req, res, ctx) => {
+			return res(ctx.status(500));
+		})
+	);
+	beforeAll(() => server.listen());
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
+
+	it('should display an unknown error appears', async () => {
+		render(<Login />);
+
+		const submitButton = screen.getByTestId('submit');
+		const email = screen.getByTestId('email');
+		fireEvent.change(email, { target: { value: 'test@test' } });
+		const password = screen.getByTestId('password');
+		fireEvent.change(password, { target: { value: '12345678' } });
+		fireEvent.click(submitButton);
+
+		await waitFor(() => {
+			expect(screen.getByText('An error occured')).toBeInTheDocument();
 		});
 	});
 });
